cf8b5b3a7975bf2a42c881f6ab66b1b659834ede
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2025-11-16 16:52:26 +0530

    ✨ Added a loader

---
+++ app/(store)/loading.tsx
@@ -0,0 +1,11 @@
+import MyLoader from "../../public/wind-toy.svg"
+
+const loading = () => {
+  return (
+    <div className="flex items-center h-screen justify-center">
+        <h1 className="text-crimson text-4xl">Forging...</h1>
+    </div>
+  )
+}
+
+export default loading
