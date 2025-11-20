b2de58de85e44f140809f4b2b569f1f75082e59d
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2025-11-20 23:49:14 +0530

    📝 Retrived orders by UserId

---
+++ sanity/lib/orders.ts
@@ -0,0 +1,41 @@
+import { defineQuery } from "next-sanity";
+import { client } from "./client";
+import { Order } from "./types";
+
+export const getOrdersByUserId = async (userId: string) => {
+  const GET_ORDERS_BY_USERID =
+    defineQuery(`*[_type == "order" && clerkUserId == $userId] | order(orderDate desc){
+  _id,
+  clerkUserId,
+  orderDate,
+  paymentMethod,
+  paymentStatus,
+  deliveryStatus,
+  totalPrice,
+  items[] {
+    quantity,
+    product->{
+      _id,
+      name,
+      price,
+      image,
+      "slug": slug.current,
+      categories[]->{
+        _id,
+        title,
+        "slug": slug.current,
+        icon
+      }
+    }
+  }
+}`);
+  try {
+    const orders = await client.fetch<Order[]>(GET_ORDERS_BY_USERID, {
+      userId,
+    });
+    return orders || [];
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return [];
+  }
+};
